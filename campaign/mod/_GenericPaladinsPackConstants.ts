a4323722d4f60c96875d2bc785cda327a87e6906
admiralnelson <admiralofinternetmeme@outlook.com>
2023-03-23 08:33:31 +0700

    added constants

--- campaign/mod/_GenericPaladinsPackConstants.ts
+++ campaign/mod/_GenericPaladinsPackConstants.ts
@@ -14,10 +14,28 @@ namespace GenericPaladinsPack {
          "wh_main_brt_bastonne", 
     ]
 
-    export const PaladinHeroAgentKeys = [
-        "admiralnelson_bret_paladin_massif_agent_key",
-        "admiralnelson_bret_paladin_massif_sword_shield_agent_key",
+    export const RESET_EACH_TURN = 10
+    export const DEBUG = true
+    
+    export const MAXIMUM_BIG_PALADINS = 6
+
+    export const RESET_POOL_COUNT_THRESHOLD = 10
+
+    export const BOT_MASSIF_PALADIN_CHANCES = 40
+    export const HUMAN_MASSIF_PALADIN_CHANCES = 15
+
+    export const BOT_2HANDED_PALADIN_CHANCES = 45
+    export const HUMAN_2HANDED_PALADIN_CHANCES = 20
+
+    export const NormalPaladins = [
         "admiralnelson_bret_paladin_2handed_agent_key"
     ]
-    
+
+    export const MassifPaladins = [
+        "admiralnelson_bret_paladin_massif_agent_key",
+        "admiralnelson_bret_paladin_massif_sword_shield_agent_key",
+    ]
+
+    export const PaladinHeroAgentKeys = NormalPaladins.concat(MassifPaladins)
+
 }
